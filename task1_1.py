55d91adfcbcf1c892519eea4428c068449b8be1b
roman <golden-roma@yandex.ru>
2021-03-08 14:05:25 +0300

    Task1_1

---
+++ task1_1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  2 20:35:22 2021
+
+@author: Роман
+"""
+
+age=9
+first_name='Иван'
+last_name='Борисов'
+print(f'Имя: {first_name} Фамилия: {last_name} Возраст: {age}')
