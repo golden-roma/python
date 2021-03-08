930b564fb16070e5e6fde28c34300623073fa5d9
roman <golden-roma@yandex.ru>
2021-03-08 14:06:08 +0300

    Task1_3

---
+++ task1_3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  3 13:35:07 2021
+
+@author: Роман
+"""
+
+
+num=input('Введите число:')
+
+n=int(num)
+nn=int(num+num)
+nnn=int(num+num+num)
+
+res=n+nn+nnn
+
+print(f'{n} + {nn} + {nnn} = {res}')
